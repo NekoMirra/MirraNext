355e2756532bebebdfc894cb3801574962bf2056
Your Name <your-email@example.com>
2025-05-03 01:41:04 +0800

    fix(Gitalk): 修复 Gitalk 初始化失败的警告信息

--- components/Gitalk.js
+++ components/Gitalk.js
@@ -12,7 +12,7 @@ const Gitalk = ({ frontMatter }) => {
   const gitalkJSCDN = siteConfig('COMMENT_GITALK_JS_CDN_URL')
   const clientId = siteConfig('COMMENT_GITALK_CLIENT_ID')
   const clientSecret = siteConfig('COMMENT_GITALK_CLIENT_SECRET')
-  const repo = siteConfig('COMMENT_GITALK_REPO')
+  const repo = "MirraNext-Talk"
   const owner = siteConfig('COMMENT_GITALK_OWNER')
   const admin = siteConfig('COMMENT_GITALK_ADMIN').split(',')
   const distractionFreeMode = siteConfig('COMMENT_GITALK_DISTRACTION_FREE_MODE')
@@ -29,9 +29,9 @@ const Gitalk = ({ frontMatter }) => {
     const gitalk = new Gitalk({
       clientID: clientId,
       clientSecret: clientSecret,
-      repo: MirraNext-Talk,
-      owner: NekoMirra,
-      admin: NekoMirra,
+      repo: repo,
+      owner: owner,
+      admin: admin,
       id: frontMatter.id, // Ensure uniqueness and length less than 50
       distractionFreeMode: distractionFreeMode // Facebook-like distraction free mode
     })
